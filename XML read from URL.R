c6a6fa3cf1eb85d5aed01507538f5a1cf862024c
Patrick <patrick.reisenthel@gmail.com>
2021-02-01 12:00:05 -0800

    now sources get_TEMP() function

--- XML read from URL.R
+++ XML read from URL.R
@@ -1,36 +1,35 @@
 # Read XML file from URL
 
+rm(list = ls())  # clear prior environment
+
 setwd("C:/Users/Patrick/Git/data-science-tools")
+source("get_TEMP.R")
 
 library(XML)
 
-URL_W   <- "https://forecast.weather.gov/MapClick.php?lat=36.17&lon=-115.33&unit=0&lg=english&FcstType=dwml"
-URL_NW  <- "https://forecast.weather.gov/MapClick.php?lat=36.29&lon=-115.30&unit=0&lg=english&FcstType=dwml"
-URL_N   <- "https://forecast.weather.gov/MapClick.php?lat=36.27&lon=-115.17&unit=0&lg=english&FcstType=dwml"
-URL_NE  <- "https://forecast.weather.gov/MapClick.php?lat=36.28&lon=-115.07&unit=0&lg=english&FcstType=dwml"
-URL_E   <- "https://forecast.weather.gov/MapClick.php?lat=36.15&lon=-115.06&unit=0&lg=english&FcstType=dwml"
-URL_SE  <- "https://forecast.weather.gov/MapClick.php?lat=36.05&lon=-115.05&unit=0&lg=english&FcstType=dwml"
-URL_S   <- "https://forecast.weather.gov/MapClick.php?lat=36.02&lon=-115.16&unit=0&lg=english&FcstType=dwml"
-URL_SW  <- "https://forecast.weather.gov/MapClick.php?lat=36.04&lon=-115.27&unit=0&lg=english&FcstType=dwml"
-URL_Ctr <- "https://forecast.weather.gov/MapClick.php?lat=36.18&lon=-115.15&unit=0&lg=english&FcstType=dwml"
+string_head <- "https://forecast.weather.gov/MapClick.php?lat="
+string_midd <- "&lon="
+string_tail <- "&unit=0&lg=english&FcstType=dwml"
 
-#fileURL <- URL_West
-#get_Temp(fileURL)
+PointNames <- c(    "W",   "NW",    "N",   "NE",    "E",   "SE",    "S",   "SW",  "Ctr")
+Longitudes <- c(-115.33,-115.30,-115.17,-115.07,-115.06,-115.05,-115.16,-115.27,-115.15)
+Latitudes  <- c(  36.17,  36.29,  36.27,  36.28,  36.15,  36.05,  36.02,  36.04,  36.18)
 
-x <- as.list(c(URL_W,URL_NW,URL_N,URL_NE,URL_E,URL_SE,URL_S,URL_SW,URL_Ctr))
-y <- lapply(x,get_Temp)
-print( as.numeric(y) )
+URLs <- vector(mode="character")  # init as zero-length character array
 
-get_Temp <- function(fileURL) {
-  
-  conn <- url(fileURL, "r")  # open connection to web page
-  on.exit(close(conn))       # close connection upon exit
-  
-  xData <- readLines(conn,warn=FALSE)
-  xmlData <- xmlParse(xData)
-  
-  maxTempForecasts <- xmlValue(getNodeSet(xmlData, "//data[@type='forecast']/parameters/temperature[@type='maximum']/value"))
-  
-  return(as.numeric(maxTempForecasts[1]))
-  
+for (i in seq_along(Longitudes)) {
+  URLs <- c( URLs, paste(string_head,as.character(Latitudes[i]),
+                         string_midd,as.character(Longitudes[i]),
+                         string_tail,sep="") )
 }
+
+names(URLs) <- PointNames
+URLs <- as.list(URLs)
+
+#print( get_Temp(URLs$W) )
+
+y <- lapply(URLs,get_Temp)  # returns list of forecasts
+print( as.numeric(y) )
+print( str(y) )
+
+
