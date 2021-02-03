d44e8d8ba82b74fcd017f4cc99f476c3e99b34c7
Patrick <patrick.reisenthel@gmail.com>
2021-02-03 09:20:34 -0800

    successful XLSX writing using openxlsx package

--- XML read from URL.R
+++ XML read from URL.R
@@ -1,27 +1,24 @@
-# Read XML file from URL and append data to existing XLSX worksheet
-
-# Note: this version (using XLConnect) works in Rstudio, but
-#       does not work in RGui.  A recommended solution is to
-#       try package:openxlsx
+# Read XML file from URL and append data to an existing XLSX worksheet
 
 rm(list = ls())  # clear prior environment
 
+library(XML)
+library(openxlsx)
 source("get_Temp.R")  # load Temp parsing function from URL
 
-library(XML)
-library(rJava)
-library(XLConnect)  # replaces (& incompatible with) package:xlsx
+# Inputs
 
-cwd <- getwd()  # current working directory
-Excelfile <- paste(cwd,"/LV Data.xlsx",sep="")
-
-string_head <- "https://forecast.weather.gov/MapClick.php?lat="
-string_midd <- "&lon="
-string_tail <- "&unit=0&lg=english&FcstType=dwml"
+Excelfile <- paste(getwd(),"/LV Data.xlsx",sep="")
 
 PointNames <- c(    "W",   "NW",    "N",   "NE",    "E",   "SE",    "S",   "SW",  "Ctr")
 Longitudes <- c(-115.33,-115.30,-115.17,-115.07,-115.06,-115.05,-115.16,-115.27,-115.15)
 Latitudes  <- c(  36.17,  36.29,  36.27,  36.28,  36.15,  36.05,  36.02,  36.04,  36.18)
+
+# Initializations
+
+string_head <- "https://forecast.weather.gov/MapClick.php?lat="
+string_midd <- "&lon="
+string_tail <- "&unit=0&lg=english&FcstType=dwml"
 
 URLs <- vector(mode="character")  # init as zero-length character array
 
@@ -31,27 +28,29 @@ for (i in seq_along(Longitudes)) {
                          string_tail,sep="") )
 }
 
+# Get forecasts list
+
 names(URLs) <- PointNames
 URLs <- as.list(URLs)  # create list of the URLs (for lapply looping)
 
 y <- lapply(URLs,get_Temp)  # returns list of forecasts
 
-today <- "2/2/2021"  # dummy character date placeholder
+# Form data frame for XLSX writing
+
+today <- "2/3/2021"  # dummy character date placeholder
 df <- data.frame(today,y)  # puts today and y into a data frame
 row.names(df) <- "maxTemp"
 print( str(df) )
 
-# The following is unsuccessful because "append = TRUE" refers to
-# the ability to add a new worksheet, not adding new data to an
-# existing worksheet.  According to Marc Schwarz on
-# https://stat.ethz.ch/pipermail/r-help/2016-January/435394.html, try
-# using the package XLConnect.  Unsuccessful attempt:
-# write.xlsx(df, Excelfile, 
-#            col.names = FALSE, row.names = FALSE, append = TRUE)
+# Append to Excelfile workbook
 
-wb <- loadWorkbook(Excelfile)  # load Excel workbook
-startRow <- nrow(readWorksheet(wb,sheet="maxTemp",header=FALSE))+1  # to append
-writeWorksheet(wb,df,sheet="maxTemp",startRow=startRow,startCol=1,header=FALSE)
-saveWorkbook(wb,Excelfile)
-xlcFreeMemory()  # attempts to solve Excel sharing violation
-
+wb <- loadWorkbook(Excelfile)
+tmp0 <-readWorkbook(wb,sheet="maxTemp",colNames=FALSE,skipEmptyRows=FALSE,skipEmptyCols=FALSE)
+startRow <- nrow(tmp0) + 1  # to append
+writeData(wb,"maxTemp",df,startRow=startRow,colNames=FALSE)
+flag <- saveWorkbook(wb,Excelfile,overwrite=TRUE,returnValue=TRUE)
+if (is.logical(flag)) {
+    print( paste("Workbook ",Excelfile," successfully updated.",sep=""))
+  } else {
+    print(flag)
+  }
